3dfa603fd2b25d6eb856a76199e38f62248c679e
root <mehershashwat@gmail.com>
2024-03-18 02:12:44 +0000

    added requirements

--- inference.py
+++ inference.py
@@ -10,12 +10,12 @@ pipe = DiffusionPipeline.from_pretrained(
 ).to("cuda")
 
 
-pipe.load_lora_weights("/workspace/lorastripedblueshirt/lorastripedblueshirt.safetensors")
+pipe.load_lora_weights("/workspace/lorastripedblueshirt/checkpoint-1000/pytorch_lora_weights.safetensors")
 
 text_encoders = [pipe.text_encoder, pipe.text_encoder_2]
 tokenizers = [pipe.tokenizer, pipe.tokenizer_2]
 
-embedding_path = "/workspace/lorastripedblueshirt/text_emb.safetensors"
+embedding_path = "/workspace/lorastripedblueshirt/checkpoint-1000/text_emb.safetensors"
 
 state_dict = load_file(embedding_path)
 # load embeddings of text_encoder 1 (CLIP ViT-L/14)
@@ -24,7 +24,7 @@ pipe.load_textual_inversion(state_dict["clip_l"], token=["<s0>", "<s1>"], text_e
 pipe.load_textual_inversion(state_dict["clip_g"], token=["<s0>", "<s1>"], text_encoder=pipe.text_encoder_2, tokenizer=pipe.tokenizer_2)
 
 instance_token = "<s0><s1>"
-prompt = f"a photo of angelina jolie wearing a {instance_token} blue striped shirt" 
+prompt = f"a photo of a woman wearing a {instance_token} blue striped shirt" 
 
-image = pipe(prompt=prompt, num_inference_steps=25, cross_attention_kwargs={"scale": 1.0}).images[0]
+image = pipe(prompt=prompt, num_inference_steps=25, cross_attention_kwargs={"scale": 0.7}).images[0]
 image.save("ang.png")
